05c2af433245be677a5064043a65c92b295b2860
CRM8000 <crm8000@qq.com>
2018-12-06 14:54:49 +0800

    工厂

---
+++ web/Application/Home/Controller/FactoryController.class.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Home\Controller;
+
+use Home\Common\FIdConst;
+use Home\Service\UserService;
+
+/**
+ * 工厂Controller
+ *
+ * @author 李静波
+ *        
+ */
+class FactoryController extends PSIBaseController {
+
+	/**
+	 * 工厂 - 主页面
+	 */
+	public function index() {
+		$us = new UserService();
+		
+		if ($us->hasPermission(FIdConst::FACTORY)) {
+			$this->initVar();
+			
+			$this->assign("title", "工厂");
+			
+			$this->display();
+		} else {
+			$this->gotoLoginPage("/Home/Factory/index");
+		}
+	}
+}

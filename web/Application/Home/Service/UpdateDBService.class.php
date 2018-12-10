abc8d400cd11fe423bd45cb463241a029659bd85
CRM8000 <crm8000@qq.com>
2018-12-10 09:52:09 +0800

    一键升级数据库 - 新增表 t_dmo_bill和t_dmo_bill_detail

--- web/Application/Home/Service/UpdateDBService.class.php
+++ web/Application/Home/Service/UpdateDBService.class.php
@@ -203,6 +203,7 @@ class UpdateDBService extends PSIBaseService {
 		$this->update_20181205_01();
 		$this->update_20181206_01();
 		$this->update_20181206_02();
+		$this->update_20181210_01();
 		
 		$sql = "delete from t_psi_db_version";
 		$db->execute($sql);
@@ -223,6 +224,70 @@ class UpdateDBService extends PSIBaseService {
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	// ============================================
 	private function notForgot() {
+	}
+
+	private function update_20181210_01() {
+		// 本次更新：新增表t_dmo_bill和t_dmo_bill_detail
+		$db = $this->db;
+		
+		// t_dmo_bill
+		$tableName = "t_dmo_bill";
+		if (! $this->tableExists($db, $tableName)) {
+			$sql = "CREATE TABLE IF NOT EXISTS `t_dmo_bill` (
+					  `id` varchar(255) NOT NULL,
+					  `ref` varchar(255) NOT NULL,
+					  `factory_id` varchar(255) NOT NULL,
+					  `contact` varchar(255) NOT NULL,
+					  `tel` varchar(255) DEFAULT NULL,
+					  `fax` varchar(255) DEFAULT NULL,
+					  `org_id` varchar(255) NOT NULL,
+					  `biz_user_id` varchar(255) NOT NULL,
+					  `biz_dt` datetime NOT NULL,
+					  `input_user_id` varchar(255) NOT NULL,
+					  `date_created` datetime DEFAULT NULL,
+					  `bill_status` int(11) NOT NULL,
+					  `goods_money` decimal(19,2) NOT NULL,
+					  `tax` decimal(19,2) NOT NULL,
+					  `money_with_tax` decimal(19,2) NOT NULL,
+					  `payment_type` int(11) NOT NULL DEFAULT 0,
+					  `deal_date` datetime NOT NULL,
+					  `deal_address` varchar(255) DEFAULT NULL,
+					  `confirm_user_id` varchar(255) DEFAULT NULL,
+					  `confirm_date` datetime DEFAULT NULL,
+					  `bill_memo` varchar(255) DEFAULT NULL,
+					  `data_org` varchar(255) DEFAULT NULL,
+					  `company_id` varchar(255) DEFAULT NULL,
+					  PRIMARY KEY (`id`)
+					) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+			";
+			$db->execute($sql);
+		}
+		
+		// t_dmo_bill_detail
+		$tableName = "t_dmo_bill_detail";
+		if (! $this->tableExists($db, $tableName)) {
+			$sql = "CREATE TABLE IF NOT EXISTS `t_dmo_bill_detail` (
+					  `id` varchar(255) NOT NULL,
+					  `dmobill_id` varchar(255) NOT NULL,
+					  `show_order` int(11) NOT NULL,
+					  `goods_id` varchar(255) NOT NULL,
+					  `goods_count` decimal(19,8) NOT NULL,
+					  `goods_money` decimal(19,2) NOT NULL,
+					  `goods_price` decimal(19,2) NOT NULL,
+					  `tax_rate` decimal(19,2) NOT NULL,
+					  `tax` decimal(19,2) NOT NULL,
+					  `money_with_tax` decimal(19,2) NOT NULL,
+					  `dmw_count` decimal(19,8) NOT NULL,
+					  `left_count` decimal(19,8) NOT NULL,
+					  `date_created` datetime DEFAULT NULL,
+					  `data_org` varchar(255) DEFAULT NULL,
+					  `company_id` varchar(255) DEFAULT NULL,
+					  `memo` varchar(255) DEFAULT NULL,
+					  PRIMARY KEY (`id`)
+					) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+			";
+			$db->execute($sql);
+		}
 	}
 
 	private function update_20181206_02() {

dee54e65ee7906f124912d0255709c93a4212762
CRM8000 <crm8000@qq.com>
2019-04-05 12:22:30 +0800

    销售日报表（按客户汇总） - 打印

--- web/Public/Scripts/PSI/Report/SaleDayByCustomerForm.js
+++ web/Public/Scripts/PSI/Report/SaleDayByCustomerForm.js
@@ -437,8 +437,57 @@ Ext.define("PSI.Report.SaleDayByCustomerForm", {
 	},
 
 	onPrint : function() {
+		var lodop = getLodop();
+		if (!lodop) {
+			PSI.MsgBox.showInfo("没有安装Lodop控件，无法打印");
+			return;
+		}
+
 		var me = this;
-		me.showInfo("TODO");
+
+		var store = me.getMainGrid().getStore();
+		var sorter = null;
+		if (store.sorters.getCount() > 0) {
+			sorter = Ext.JSON.encode([store.sorters.getAt(0)]);
+		}
+
+		var el = Ext.getBody();
+		el.mask("数据加载中...");
+		var r = {
+			url : PSI.Const.BASE_URL
+					+ "Home/Report/genSaleDayByCustomerPrintPage",
+			params : {
+				dt : Ext.Date.format(Ext.getCmp("editQueryDT").getValue(),
+						"Y-m-d"),
+				sort : sorter,
+				limit : -1
+			},
+			callback : function(options, success, response) {
+				el.unmask();
+
+				if (success) {
+					var data = response.responseText;
+					me.printReport("销售日报表(按客户汇总)", data);
+				}
+			}
+		};
+		me.ajax(r);
+	},
+
+	printReport : function(ref, data) {
+		var me = this;
+
+		var lodop = getLodop();
+		if (!lodop) {
+			PSI.MsgBox.showInfo("Lodop打印控件没有正确安装");
+			return;
+		}
+
+		lodop.PRINT_INIT(ref);
+		lodop.SET_PRINT_PAGESIZE(1, me.PRINT_PAGE_WIDTH, me.PRINT_PAGE_HEIGHT,
+				"");
+		lodop.ADD_PRINT_HTM("0mm", "0mm", "100%", "100%", data);
+		var result = lodop.PRINT();
 	},
 
 	onPDF : function() {

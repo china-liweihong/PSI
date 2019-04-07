c7585022dd4b9c1408526450cd7d07fbb6a78032
CRM8000 <crm8000@qq.com>
2019-04-07 10:21:47 +0800

    销售日报表（按商品汇总） - 导出PDF和Excel的文件名加上时间戳后缀

--- web/Application/Home/Service/SaleReportService.class.php
+++ web/Application/Home/Service/SaleReportService.class.php
@@ -273,8 +273,10 @@ class SaleReportService extends PSIBaseExService {
 		$lastRow = count($items) + 2;
 		$sheet->getStyle('A2:L' . $lastRow)->applyFromArray($styleArray);
 		
+		$dt = date("YmdHis");
+		
 		header('Content-Type: application/vnd.ms-excel');
-		header('Content-Disposition: attachment;filename="SDG.xlsx"');
+		header('Content-Disposition: attachment;filename="SDG_' . $dt . '.xlsx"');
 		header('Cache-Control: max-age=0');
 		
 		$writer = \PHPExcel_IOFactory::createWriter($excel, "Excel2007");

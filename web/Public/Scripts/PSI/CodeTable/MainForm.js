b32ae75e8bfbee899cc1a3c560c7ef84fcb3b771
CRM8000 <crm8000@qq.com>
2019-04-16 15:27:33 +0800

    码表设置

--- web/Public/Scripts/PSI/CodeTable/MainForm.js
+++ web/Public/Scripts/PSI/CodeTable/MainForm.js
@@ -19,19 +19,20 @@ Ext.define("PSI.CodeTable.MainForm", {
 													region : "center",
 													xtype : "panel",
 													layout : "fit",
-													//border : 0,
+													// border : 0,
 													items : []
 												}, {
 													id : "panelCategory",
 													xtype : "panel",
 													region : "west",
 													layout : "fit",
-													width : 430,
+													width : 300,
 													split : true,
 													collapsible : true,
 													header : false,
-													//border : 0,
-													items : []
+													border : 0,
+													items : [me
+															.getCategoryGrid()]
 												}]
 									}]
 						});
@@ -43,6 +44,10 @@ Ext.define("PSI.CodeTable.MainForm", {
 				var me = this;
 
 				return [{
+							text : "新增码表分类",
+							handler : me.onAddCategory,
+							scope : me
+						}, "-", {
 							text : "帮助",
 							handler : function() {
 								me.showInfo("TODO");
@@ -53,5 +58,70 @@ Ext.define("PSI.CodeTable.MainForm", {
 								me.closeWindow();
 							}
 						}];
+			},
+
+			getCategoryGrid : function() {
+				var me = this;
+
+				if (me.__categoryGrid) {
+					return me.__categoryGrid;
+				}
+
+				var modelName = "PSICodeTableCategory";
+
+				Ext.define(modelName, {
+							extend : "Ext.data.Model",
+							fields : ["id", "code", "name"]
+						});
+
+				me.__categoryGrid = Ext.create("Ext.grid.Panel", {
+							cls : "PSI",
+							viewConfig : {
+								enableTextSelection : true
+							},
+							header : {
+								height : 30,
+								title : me.formatGridHeaderTitle("码表分类")
+							},
+							tools : [{
+										type : "close",
+										handler : function() {
+											Ext.getCmp("panelCategory")
+													.collapse();
+										}
+									}],
+							columnLines : true,
+							columns : [{
+										header : "分类编码",
+										dataIndex : "code",
+										width : 80,
+										menuDisabled : true,
+										sortable : false
+									}, {
+										header : "码表分类",
+										dataIndex : "name",
+										width : 200,
+										menuDisabled : true,
+										sortable : false
+									}],
+							store : Ext.create("Ext.data.Store", {
+										model : modelName,
+										autoLoad : false,
+										data : []
+									}),
+							listeners : {
+								select : {
+									fn : me.onCategoryGridSelect,
+									scope : me
+								}
+							}
+						});
+
+				return me.__categoryGrid;
+			},
+
+			onAddCategory : function() {
+				var me = this;
+				me.showInfo("TODO");
 			}
 		});

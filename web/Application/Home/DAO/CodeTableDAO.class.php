e5cade0c1b30991006d66d5a4e08c72d4a7e240e
CRM8000 <crm8000@qq.com>
2019-04-17 09:44:22 +0800

    码表设置

--- web/Application/Home/DAO/CodeTableDAO.class.php
+++ web/Application/Home/DAO/CodeTableDAO.class.php
@@ -39,6 +39,42 @@ class CodeTableDAO extends PSIBaseExDAO {
 	 * @return null|array
 	 */
 	public function addCodeTableCategory(& $params) {
-		return $this->todo();
+		$db = $this->db;
+		
+		$code = $params["code"];
+		$name = $params["name"];
+		
+		// 检查编码是否存在
+		if ($code) {
+			$sql = "select count(*) as cnt from t_code_table_category where code = '%s' ";
+			$data = $db->query($sql, $code);
+			$cnt = $data[0]["cnt"];
+			if ($cnt) {
+				return $this->bad("码表分类编码[{$code}]已经存在");
+			}
+		} else {
+			$code = "";
+		}
+		
+		// 检查分类名称是否存在
+		$sql = "select count(*) as cnt from t_code_table_category where name = '%s' ";
+		$data = $db->query($sql, $name);
+		$cnt = $data[0]["cnt"];
+		if ($cnt) {
+			return $this->bad("码表分类[{$name}]已经存在");
+		}
+		
+		$id = $this->newId();
+		$sql = "insert into t_code_table_category (id, code, name, parent_id)
+				values ('%s', '%s', '%s', null)";
+		
+		$rc = $db->execute($sql, $id, $code, $name);
+		if ($rc === false) {
+			return $this->sqlError(__METHOD__, __LINE__);
+		}
+		
+		// 操作成功
+		$params["id"] = $id;
+		return null;
 	}
 }

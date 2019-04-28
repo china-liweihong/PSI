e179eef9041b36a2fe938703c361e642f337419e
CRM8000 <crm8000@qq.com>
2019-04-28 12:39:07 +0800

    更新数据库文档 - 初始化t_sysdict_record_status的数据

--- web/Application/Home/Service/InstallService.class.php
+++ web/Application/Home/Service/InstallService.class.php
@@ -2209,6 +2209,25 @@ class InstallService extends PSIBaseExService {
 		";
 		$db->execute($sql);
 		
+		// t_dict_table_category
+		$sql = "INSERT INTO `t_dict_table_category` (`id`, `code`, `name`, `parent_id`) VALUES
+				('01', '01', '码表', NULL);
+		";
+		$db->execute($sql);
+		
+		// t_dict_table_md
+		$sql = "INSERT INTO `t_dict_table_md` (`id`, `code`, `name`, `table_name`, `category_id`, `memo`, `py`) VALUES
+				('01', '01', '码表记录状态', 't_sysdict_record_status', '01', '用于码表', 'MBJLZT');
+		";
+		$db->execute($sql);
+		
+		// t_sysdict_record_status
+		$sql = "INSERT INTO `t_sysdict_record_status` (`id`, `code`, `codeInt`, `name`, `py`, `memo`) VALUES
+				('9B90C56E-696E-11E9-B2BF-782BCBD7746B', '1', 1, '启用', 'QY', '记录处于启用状态'),
+				('AC7F3FAB-696E-11E9-B2BF-782BCBD7746B', '2', 2, '停用', 'TY', '记录处于停用状态');
+		";
+		$db->execute($sql);
+		
 		// t_psi_db_version
 		$sql = "INSERT INTO `t_psi_db_version` (`db_version`, `update_dt`) VALUES
 					('%s', now());

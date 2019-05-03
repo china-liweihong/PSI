5912a3e11fda6660445bd15556fc527d8c2d6d40
CRM8000 <crm8000@qq.com>
2019-05-03 18:17:09 +0800

    更新数据库文档 - t_code_table_md新增字段md_version和is_fixed

--- web/Application/Home/Service/InstallService.class.php
+++ web/Application/Home/Service/InstallService.class.php
@@ -1573,6 +1573,8 @@ class InstallService extends PSIBaseExService {
 				  `memo` varchar(1000) DEFAULT NULL,
 				  `py` varchar(255) DEFAULT NULL,
 				  `fid` varchar(255) DEFAULT NULL,
+				  `md_version` int(11) NOT NULL DEFAULT 1,
+				  `is_fixed` int(11) NOT NULL DEFAULT 2,
 				  PRIMARY KEY (`id`)
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 		";

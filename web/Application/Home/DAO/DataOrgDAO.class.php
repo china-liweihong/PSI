b8b56b6092a8dc9a8099713d63dbb5643c4f3e0c
CRM8000 <crm8000@qq.com>
2019-05-07 09:37:52 +0800

    代码整理

--- web/Application/Home/DAO/DataOrgDAO.class.php
+++ web/Application/Home/DAO/DataOrgDAO.class.php
@@ -13,7 +13,7 @@ class DataOrgDAO extends PSIBaseExDAO {
 
 	private function getLoginUserDataOrg($loginUserId) {
 		$db = $this->db;
-		
+
 		$sql = "select data_org from t_user where id = '%s' ";
 		$data = $db->query($sql, $loginUserId);
 		if ($data) {
@@ -25,15 +25,15 @@ class DataOrgDAO extends PSIBaseExDAO {
 
 	private function getDataOrgForFId($fid, $loginUserId) {
 		$db = $this->db;
-		
+
 		$result = [];
-		
+
 		if ($loginUserId == DemoConst::ADMIN_USER_ID) {
 			// admin 是超级管理员
 			$result[] = "*";
 			return $result;
 		}
-		
+
 		$sql = "select distinct rpd.data_org
 				from t_role_permission rp, t_role_permission_dataorg rpd,
 					t_role_user ru
@@ -41,11 +41,11 @@ class DataOrgDAO extends PSIBaseExDAO {
 					and rp.role_id = rpd.role_id and rp.permission_id = rpd.permission_id
 					and rpd.permission_id = '%s' ";
 		$data = $db->query($sql, $loginUserId, $fid);
-		
+
 		foreach ( $data as $v ) {
 			$result[] = $v["data_org"];
 		}
-		
+
 		return $result;
 	}
 
@@ -54,37 +54,37 @@ class DataOrgDAO extends PSIBaseExDAO {
 	 */
 	public function buildSQL($fid, $tableName, $loginUserId) {
 		$queryParams = [];
-		
+
 		$userDataOrg = $this->getLoginUserDataOrg($loginUserId);
-		
+
 		$dataOrgList = $this->getDataOrgForFId($fid, $loginUserId);
-		
+
 		if (count($dataOrgList) == 0) {
 			return null; // 全部数据域
 		}
-		
+
 		// data_org is null 是为了兼容之前的版本遗留下的数据
 		$result = " ( " . $tableName . ".data_org is null or " . $tableName . ".data_org = '' ";
-		foreach ( $dataOrgList as $i => $dataOrg ) {
+		foreach ( $dataOrgList as $dataOrg ) {
 			if ($dataOrg == "*") {
 				return null; // 全部数据域
 			}
-			
+
 			// # 表示是当前用户自身的数据域
 			if ($dataOrg == "#") {
 				$result .= " or " . $tableName . ".data_org = '%s' ";
 				$queryParams[] = $userDataOrg;
-				
+
 				continue;
 			}
-			
+
 			$result .= " or left(" . $tableName . ".data_org, %d) = '%s' ";
 			$queryParams[] = strlen($dataOrg);
 			$queryParams[] = $dataOrg;
 		}
-		
+
 		$result .= " ) ";
-		
+
 		return [
 				0 => $result,
 				1 => $queryParams

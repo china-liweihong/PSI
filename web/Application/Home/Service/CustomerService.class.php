c8b2fce77ff80d68828a627670f811e253b2d7c4
CRM8000 <crm8000@qq.com>
2019-05-08 14:55:04 +0800

    代码整理

--- web/Application/Home/Service/CustomerService.class.php
+++ web/Application/Home/Service/CustomerService.class.php
@@ -15,16 +15,16 @@ class CustomerService extends PSIBaseExService {
 	/**
 	 * 客户分类列表
 	 *
-	 * @param array $params        	
+	 * @param array $params
 	 * @return array
 	 */
 	public function categoryList($params) {
 		if ($this->isNotOnline()) {
 			return $this->emptyResult();
 		}
-		
+
 		$params["loginUserId"] = $this->getLoginUserId();
-		
+
 		$dao = new CustomerDAO($this->db());
 		return $dao->categoryList($params);
 	}
@@ -36,18 +36,18 @@ class CustomerService extends PSIBaseExService {
 		if ($this->isNotOnline()) {
 			return $this->notOnlineError();
 		}
-		
+
 		$id = $params["id"];
 		$code = $params["code"];
 		$name = $params["name"];
-		
+
 		$db = $this->db();
 		$db->startTrans();
-		
+
 		$dao = new CustomerDAO($db);
-		
+
 		$log = null;
-		
+
 		if ($id) {
 			// 编辑
 			$rc = $dao->updateCustomerCategory($params);
@@ -55,101 +55,99 @@ class CustomerService extends PSIBaseExService {
 				$db->rollback();
 				return $rc;
 			}
-			
+
 			$log = "编辑客户分类: 编码 = {$code}, 分类名 = {$name}";
 		} else {
 			// 新增
-			
+
 			$params["dataOrg"] = $this->getLoginUserDataOrg();
 			$params["companyId"] = $this->getCompanyId();
-			
+
 			$rc = $dao->addCustomerCategory($params);
 			if ($rc) {
 				$db->rollback();
 				return $rc;
 			}
-			
+
 			$id = $params["id"];
-			
+
 			$log = "新增客户分类：编码 = {$code}, 分类名 = {$name}";
 		}
-		
+
 		// 记录业务日志
 		$bs = new BizlogService($db);
 		$bs->insertBizlog($log, $this->LOG_CATEGORY);
-		
+
 		$db->commit();
-		
+
 		return $this->ok($id);
 	}
 
 	/**
 	 * 删除客户分类
 	 *
-	 * @param array $params        	
+	 * @param array $params
 	 * @return array
 	 */
 	public function deleteCategory($params) {
 		if ($this->isNotOnline()) {
 			return $this->notOnlineError();
 		}
-		
-		$id = $params["id"];
-		
+
 		$db = $this->db();
 		$db->startTrans();
-		
+
 		$dao = new CustomerDAO($db);
-		
+
 		$rc = $dao->deleteCustomerCategory($params);
 		if ($rc) {
 			$db->rollback();
 			return $rc;
 		}
-		
+
 		$log = "删除客户分类： 编码 = {$params['code']}, 分类名称 = {$params['name']}";
 		$bs = new BizlogService($db);
 		$bs->insertBizlog($log, $this->LOG_CATEGORY);
-		
+
 		$db->commit();
-		
+
 		return $this->ok();
 	}
 
 	/**
 	 * 新建或编辑客户资料
 	 *
-	 * @param array $params        	
+	 * @param array $params
 	 * @return array
 	 */
 	public function editCustomer($params) {
 		if ($this->isNotOnline()) {
 			return $this->notOnlineError();
 		}
-		
+
 		$id = $params["id"];
 		$code = $params["code"];
 		$name = $params["name"];
-		
+
 		$ps = new PinyinService();
 		$params["py"] = $ps->toPY($name);
-		
+
 		$db = $this->db();
 		$db->startTrans();
-		
+
 		$dao = new CustomerDAO($db);
-		
+
 		$params["dataOrg"] = $this->getLoginUserDataOrg();
 		$params["companyId"] = $this->getCompanyId();
-		
+
 		$category = $dao->getCustomerCategoryById($params["categoryId"]);
 		if (! $category) {
 			$db->rollback();
 			return $this->bad("客户分类不存在");
 		}
-		
+
 		$log = null;
-		
+
 		if ($id) {
 			// 编辑
 			$rc = $dao->updateCustomer($params);
@@ -157,7 +155,7 @@ class CustomerService extends PSIBaseExService {
 				$db->rollback();
 				return $rc;
 			}
-			
+
 			$log = "编辑客户：编码 = {$code}, 名称 = {$name}";
 		} else {
 			// 新增
@@ -166,41 +164,41 @@ class CustomerService extends PSIBaseExService {
 				$db->rollback();
 				return $rc;
 			}
-			
+
 			$id = $params["id"];
-			
+
 			$log = "新增客户：编码 = {$code}, 名称 = {$name}";
 		}
-		
+
 		// 处理应收账款
 		$rc = $dao->initReceivables($params);
 		if ($rc) {
 			$db->rollback();
 			return $rc;
 		}
-		
+
 		// 记录业务日志
 		$bs = new BizlogService($db);
 		$bs->insertBizlog($log, $this->LOG_CATEGORY);
-		
+
 		$db->commit();
-		
+
 		return $this->ok($id);
 	}
 
 	/**
 	 * 获得某个分类的客户列表
 	 *
-	 * @param array $params        	
+	 * @param array $params
 	 * @return array
 	 */
 	public function customerList($params) {
 		if ($this->isNotOnline()) {
 			return $this->emptyResult();
 		}
-		
+
 		$params["loginUserId"] = $this->getLoginUserId();
-		
+
 		$dao = new CustomerDAO($this->db());
 		return $dao->customerList($params);
 	}
@@ -208,50 +206,49 @@ class CustomerService extends PSIBaseExService {
 	/**
 	 * 删除客户资料
 	 *
-	 * @param array $params        	
+	 * @param array $params
 	 * @return array
 	 */
 	public function deleteCustomer($params) {
 		if ($this->isNotOnline()) {
 			return $this->notOnlineError();
 		}
-		
-		$id = $params["id"];
+
 		$db = $this->db();
 		$db->startTrans();
-		
+
 		$dao = new CustomerDAO($db);
-		
+
 		$rc = $dao->deleteCustomer($params);
 		if ($rc) {
 			$db->rollback();
 			return $rc;
 		}
-		
+
 		$code = $params["code"];
 		$name = $params["name"];
 		$log = "删除客户资料：编码 = {$code},  名称 = {$name}";
 		$bs = new BizlogService($db);
 		$bs->insertBizlog($log, $this->LOG_CATEGORY);
-		
+
 		$db->commit();
-		
+
 		return $this->ok();
 	}
 
 	/**
 	 * 客户字段，查询数据
 	 *
-	 * @param array $params        	
+	 * @param array $params
 	 * @return array
 	 */
 	public function queryData($params) {
 		if ($this->isNotOnline()) {
 			return $this->emptyResult();
 		}
-		
+
 		$params["loginUserId"] = $this->getLoginUserId();
-		
+
 		$dao = new CustomerDAO($this->db());
 		return $dao->queryData($params);
 	}
@@ -267,7 +264,7 @@ class CustomerService extends PSIBaseExService {
 		if ($this->isNotOnline()) {
 			return $this->emptyResult();
 		}
-		
+
 		$dao = new CustomerDAO($this->db());
 		return $dao->customerInfo($id);
 	}
@@ -275,15 +272,15 @@ class CustomerService extends PSIBaseExService {
 	/**
 	 * 判断给定id的客户是否存在
 	 *
-	 * @param string $customerId        	
+	 * @param string $customerId
 	 *
 	 * @return true: 存在
 	 */
 	public function customerExists($customerId, $db) {
 		$dao = new CustomerDAO($db);
-		
+
 		$customer = $dao->getCustomerById($customerId);
-		
+
 		return $customer != null;
 	}
 
@@ -292,7 +289,7 @@ class CustomerService extends PSIBaseExService {
 	 */
 	public function getCustomerNameById($customerId, $db) {
 		$dao = new CustomerDAO($db);
-		
+
 		$customer = $dao->getCustomerById($customerId);
 		if ($customer) {
 			return $customer["name"];
@@ -308,7 +305,7 @@ class CustomerService extends PSIBaseExService {
 		if ($this->isNotOnline()) {
 			return $this->emptyResult();
 		}
-		
+
 		$dao = new CustomerDAO($this->db());
 		return $dao->priceSystemList($params);
 	}

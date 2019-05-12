9b939b38c1990673a7db955237bf85ee492924c4
CRM8000 <crm8000@qq.com>
2019-05-12 18:36:12 +0800

    代码整理

--- web/Application/Home/Service/DataOrgService.class.php
+++ web/Application/Home/Service/DataOrgService.class.php
@@ -14,36 +14,36 @@ class DataOrgService extends PSIBaseService {
 	 */
 	public function buildSQL($fid, $tableName) {
 		$queryParams = array();
-		
+
 		$us = new UserService();
 		$userDataOrg = $us->getLoginUserDataOrg();
-		
+
 		$dataOrgList = $us->getDataOrgForFId($fid);
 		if (count($dataOrgList) == 0) {
 			return null; // 全部数据域
 		}
-		
+
 		// data_org is null 是为了兼容之前的版本遗留下的数据
 		$result = " ( " . $tableName . ".data_org is null or " . $tableName . ".data_org = '' ";
-		foreach ( $dataOrgList as $i => $dataOrg ) {
+		foreach ( $dataOrgList as $dataOrg ) {
 			if ($dataOrg == "*") {
 				return null; // 全部数据域
 			}
-			
+
 			if ($dataOrg == "#") {
 				$result .= " or " . $tableName . ".data_org = '%s' ";
 				$queryParams[] = $userDataOrg;
-				
+
 				continue;
 			}
-			
+
 			$result .= " or left(" . $tableName . ".data_org, %d) = '%s' ";
 			$queryParams[] = strlen($dataOrg);
 			$queryParams[] = $dataOrg;
 		}
-		
+
 		$result .= " ) ";
-		
+
 		return array(
 				0 => $result,
 				1 => $queryParams

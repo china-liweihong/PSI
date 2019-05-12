c69228850d32b6e3241dc5a0f41bb0edc5f540c9
CRM8000 <crm8000@qq.com>
2019-05-12 18:46:40 +0800

    代码整理

--- web/Application/Home/Service/InitInventoryService.class.php
+++ web/Application/Home/Service/InitInventoryService.class.php
@@ -20,19 +20,19 @@ class InitInventoryService extends PSIBaseExService {
 		if ($this->isNotOnline()) {
 			return $this->emptyResult();
 		}
-		
+
 		$sql = "select id, code, name, inited from t_warehouse ";
 		$queryParams = array();
-		
+
 		$ds = new DataOrgService();
 		$rs = $ds->buildSQL(FIdConst::INVENTORY_INIT, "t_warehouse");
 		if ($rs) {
 			$sql .= " where " . $rs[0];
 			$queryParams = $rs[1];
 		}
-		
+
 		$sql .= "order by code";
-		
+
 		return M()->query($sql, $queryParams);
 	}
 
@@ -43,19 +43,18 @@ class InitInventoryService extends PSIBaseExService {
 		if ($this->isNotOnline()) {
 			return $this->emptyResult();
 		}
-		
+
 		$warehouseId = $params["warehouseId"];
-		$page = $params["page"];
 		$start = $params["start"];
 		$limit = $params["limit"];
-		
+
 		$db = M();
-		
+
 		$companyId = $this->getCompanyId();
 		$bcDAO = new BizConfigDAO($db);
 		$dataScale = $bcDAO->getGoodsCountDecNumber($companyId);
 		$fmt = "decimal(19, " . $dataScale . ")";
-		
+
 		$sql = "select v.id, g.code, g.name, g.spec, convert(v.balance_count, $fmt) as balance_count, v.balance_price, 
 				v.balance_money, u.name as unit_name, v.biz_date 
 				from t_inventory_detail v, t_goods g, t_goods_unit u 
@@ -76,11 +75,11 @@ class InitInventoryService extends PSIBaseExService {
 			$result[$i]["goodsPrice"] = $v["balance_price"];
 			$result[$i]["initDate"] = date("Y-m-d", strtotime($v["biz_date"]));
 		}
-		
+
 		$sql = "select count(*) as cnt from t_inventory_detail 
 				where warehouse_id = '%s' and ref_type = '库存建账' ";
 		$data = $db->query($sql, $warehouseId);
-		
+
 		return array(
 				"initInfoList" => $result,
 				"totalCount" => $data[0]["cnt"]
@@ -94,19 +93,19 @@ class InitInventoryService extends PSIBaseExService {
 		if ($this->isNotOnline()) {
 			return $this->emptyResult();
 		}
-		
+
 		$sql = "select id, code, name, full_name from t_goods_category ";
 		$queryParams = array();
-		
+
 		$ds = new DataOrgService();
 		$rs = $ds->buildSQL(FIdConst::INVENTORY_INIT, "t_goods_category");
 		if ($rs) {
 			$sql .= " where " . $rs[0];
 			$queryParams = $rs[1];
 		}
-		
+
 		$sql .= " order by code";
-		
+
 		$result = array();
 		$db = M();
 		$data = $db->query($sql, $queryParams);
@@ -119,7 +118,7 @@ class InitInventoryService extends PSIBaseExService {
 			}
 			$result[$i]["name"] = $fullName;
 		}
-		
+
 		return $result;
 	}
 
@@ -130,21 +129,20 @@ class InitInventoryService extends PSIBaseExService {
 		if ($this->isNotOnline()) {
 			return $this->emptyResult();
 		}
-		
+
 		$warehouseId = $params["warehouseId"];
 		$categoryId = $params["categoryId"];
-		$page = $params["page"];
 		$start = $params["start"];
 		$limit = $params["limit"];
-		
+
 		$db = M();
-		
+
 		$companyId = $this->getCompanyId();
-		
+
 		$bcDAO = new BizConfigDAO($db);
 		$dataScale = $bcDAO->getGoodsCountDecNumber($companyId);
 		$fmt = "decimal(19, " . $dataScale . ")";
-		
+
 		$sql = "select g.id, g.code, g.name, g.spec, convert(v.balance_count, $fmt) as balance_count, v.balance_price, 
 				v.balance_money, u.name as unit_name, v.biz_date 
 				from t_goods g inner join t_goods_unit u 
@@ -167,10 +165,10 @@ class InitInventoryService extends PSIBaseExService {
 			$result[$i]["goodsPrice"] = $v["balance_price"];
 			$result[$i]["initDate"] = $v["biz_date"];
 		}
-		
+
 		$sql = "select count(*) as cnt from t_goods where category_id = '%s' ";
 		$data = $db->query($sql, $categoryId);
-		
+
 		return array(
 				"goodsList" => $result,
 				"totalCount" => $data[0]["cnt"]
@@ -180,12 +178,10 @@ class InitInventoryService extends PSIBaseExService {
 	private function deleteInitInventoryGoods($params) {
 		$warehouseId = $params["warehouseId"];
 		$goodsId = $params["goodsId"];
-		$goodsCount = $params["goodsCount"];
-		$goodsMoney = floatval($params["goodsMoney"]);
-		
+
 		$db = M();
 		$db->startTrans();
-		
+
 		$sql = "select name, inited from t_warehouse where id = '%s' ";
 		$data = $db->query($sql, $warehouseId);
 		if (! $data) {
@@ -196,7 +192,7 @@ class InitInventoryService extends PSIBaseExService {
 			$db->rollback();
 			return $this->bad("仓库 [{$data[0]["name"]}] 已经建账完成，不能再次建账");
 		}
-		
+
 		$sql = "select name from t_goods where id = '%s' ";
 		$data = $db->query($sql, $goodsId);
 		if (! $data) {
@@ -211,7 +207,7 @@ class InitInventoryService extends PSIBaseExService {
 			$db->rollback();
 			return $this->bad("当前商品已经有业务发生，不能再建账");
 		}
-		
+
 		// 清除明细账中记录
 		$sql = "delete from t_inventory_detail
 				where warehouse_id = '%s' and goods_id = '%s' ";
@@ -220,7 +216,7 @@ class InitInventoryService extends PSIBaseExService {
 			$db->rollback();
 			return $this->sqlError(__LINE__);
 		}
-		
+
 		// 清除总账中记录
 		$sql = "delete from t_inventory
 				where warehouse_id = '%s' and goods_id = '%s' ";
@@ -229,9 +225,9 @@ class InitInventoryService extends PSIBaseExService {
 			$db->rollback();
 			return $this->sqlError(__LINE__);
 		}
-		
+
 		$db->commit();
-		
+
 		return $this->ok();
 	}
 
@@ -242,29 +238,29 @@ class InitInventoryService extends PSIBaseExService {
 		if ($this->isNotOnline()) {
 			return $this->notOnlineError();
 		}
-		
+
 		$warehouseId = $params["warehouseId"];
 		$goodsId = $params["goodsId"];
 		$goodsCount = floatval($params["goodsCount"]);
 		$goodsMoney = floatval($params["goodsMoney"]);
-		
+
 		if ($goodsCount < 0) {
 			return $this->bad("期初数量不能为负数");
 		}
-		
+
 		if ($goodsMoney < 0) {
 			return $this->bad("期初金额不能为负数");
 		}
-		
+
 		if (abs($goodsCount) <= 0.000000001) {
 			return $this->deleteInitInventoryGoods($params);
 		}
-		
+
 		$goodsPrice = $goodsMoney / $goodsCount;
-		
+
 		$db = M();
 		$db->startTrans();
-		
+
 		$sql = "select name, inited from t_warehouse where id = '%s' ";
 		$data = $db->query($sql, $warehouseId);
 		if (! $data) {
@@ -275,7 +271,7 @@ class InitInventoryService extends PSIBaseExService {
 			$db->rollback();
 			return $this->bad("仓库 [{$data[0]["name"]}] 已经建账完成，不能再次建账");
 		}
-		
+
 		$sql = "select name from t_goods where id = '%s' ";
 		$data = $db->query($sql, $goodsId);
 		if (! $data) {
@@ -290,16 +286,16 @@ class InitInventoryService extends PSIBaseExService {
 			$db->rollback();
 			return $this->bad("当前商品已经有业务发生，不能再建账");
 		}
-		
+
 		$us = new UserService();
 		$dataOrg = $us->getLoginUserDataOrg();
-		
+
 		$companyId = $this->getCompanyId();
-		
+
 		$bcDAO = new BizConfigDAO($db);
 		$dataScale = $bcDAO->getGoodsCountDecNumber($companyId);
 		$fmt = "decimal(19, " . $dataScale . ")";
-		
+
 		// 总账
 		$sql = "select id from t_inventory where warehouse_id = '%s' and goods_id = '%s' ";
 		$data = $db->query($sql, $warehouseId, $goodsId);
@@ -307,7 +303,7 @@ class InitInventoryService extends PSIBaseExService {
 			$sql = "insert into t_inventory (warehouse_id, goods_id, in_count, in_price, 
 						in_money, balance_count, balance_price, balance_money, data_org) 
 						values ('%s', '%s', convert(%f, $fmt), %f, %f, convert(%f, $fmt), %f, %f, '%s') ";
-			$rc = $db->execute($sql, $warehouseId, $goodsId, $goodsCount, $goodsPrice, $goodsMoney, 
+			$rc = $db->execute($sql, $warehouseId, $goodsId, $goodsCount, $goodsPrice, $goodsMoney,
 					$goodsCount, $goodsPrice, $goodsMoney, $dataOrg);
 			if ($rc === false) {
 				$db->rollback();
@@ -319,14 +315,14 @@ class InitInventoryService extends PSIBaseExService {
 						set in_count = convert(%f, $fmt), in_price = %f, in_money = %f, 
 						balance_count = convert(%f, $fmt), balance_price = %f, balance_money = %f 
 						where id = %d ";
-			$rc = $db->execute($sql, $goodsCount, $goodsPrice, $goodsMoney, $goodsCount, 
-					$goodsPrice, $goodsMoney, $id);
+			$rc = $db->execute($sql, $goodsCount, $goodsPrice, $goodsMoney, $goodsCount, $goodsPrice,
+					$goodsMoney, $id);
 			if ($rc === false) {
 				$db->rollback();
 				return $this->sqlError(__LINE__);
 			}
 		}
-		
+
 		// 明细账
 		$sql = "select id from t_inventory_detail  
 					where warehouse_id = '%s' and goods_id = '%s' and ref_type = '库存建账' ";
@@ -337,7 +333,7 @@ class InitInventoryService extends PSIBaseExService {
 						biz_date, biz_user_id, date_created,  ref_number, ref_type, data_org)
 						values ('%s', '%s', convert(%f, $fmt), %f, %f, convert(%f, $fmt), 
 								%f, %f, curdate(), '%s', now(), '', '库存建账', '%s')";
-			$rc = $db->execute($sql, $warehouseId, $goodsId, $goodsCount, $goodsPrice, $goodsMoney, 
+			$rc = $db->execute($sql, $warehouseId, $goodsId, $goodsCount, $goodsPrice, $goodsMoney,
 					$goodsCount, $goodsPrice, $goodsMoney, $us->getLoginUserId(), $dataOrg);
 			if ($rc === false) {
 				$db->rollback();
@@ -350,16 +346,16 @@ class InitInventoryService extends PSIBaseExService {
 						balance_count = convert(%f, $fmt), balance_price = %f, balance_money = %f,
 						biz_date = curdate()  
 						where id = %d ";
-			$rc = $db->execute($sql, $goodsCount, $goodsPrice, $goodsMoney, $goodsCount, 
-					$goodsPrice, $goodsMoney, $id);
+			$rc = $db->execute($sql, $goodsCount, $goodsPrice, $goodsMoney, $goodsCount, $goodsPrice,
+					$goodsMoney, $id);
 			if ($rc === false) {
 				$db->rollback();
 				return $this->sqlError(__LINE__);
 			}
 		}
-		
+
 		$db->commit();
-		
+
 		return $this->ok();
 	}
 
@@ -370,12 +366,12 @@ class InitInventoryService extends PSIBaseExService {
 		if ($this->isNotOnline()) {
 			return $this->notOnlineError();
 		}
-		
+
 		$warehouseId = $params["warehouseId"];
-		
+
 		$db = M();
 		$db->startTrans();
-		
+
 		$sql = "select name, inited from t_warehouse where id = '%s' ";
 		$data = $db->query($sql, $warehouseId);
 		if (! $data) {
@@ -388,14 +384,14 @@ class InitInventoryService extends PSIBaseExService {
 			$db->rollback();
 			return $this->bad("仓库 [{$name}] 已经建账完毕");
 		}
-		
+
 		$sql = "update t_warehouse set inited = 1 where id = '%s' ";
 		$rc = $db->execute($sql, $warehouseId);
 		if ($rc === false) {
 			$db->rollback();
 			return $this->sqlError(__LINE__);
 		}
-		
+
 		$sql = "update t_inventory_detail set biz_date = curdate() 
 					where warehouse_id = '%s' and ref_type = '库存建账' ";
 		$rc = $db->execute($sql, $warehouseId);
@@ -403,13 +399,13 @@ class InitInventoryService extends PSIBaseExService {
 			$db->rollback();
 			return $this->sqlError(__LINE__);
 		}
-		
+
 		$log = "仓库 [{$name}] 建账完毕";
 		$bs = new BizlogService();
 		$bs->insertBizlog($log, $this->LOG_CATEGORY);
-		
+
 		$db->commit();
-		
+
 		return $this->ok();
 	}
 
@@ -420,12 +416,12 @@ class InitInventoryService extends PSIBaseExService {
 		if ($this->isNotOnline()) {
 			return $this->notOnlineError();
 		}
-		
+
 		$warehouseId = $params["warehouseId"];
-		
+
 		$db = M();
 		$db->startTrans();
-		
+
 		$sql = "select name, inited from t_warehouse where id = '%s' ";
 		$data = $db->query($sql, $warehouseId);
 		if (! $data) {
@@ -446,20 +442,20 @@ class InitInventoryService extends PSIBaseExService {
 			$db->rollback();
 			return $this->bad("仓库 [{$name}] 中已经发生出入库业务，不能再取消建账标志");
 		}
-		
+
 		$sql = "update t_warehouse set inited = 0 where id = '%s' ";
 		$rc = $db->execute($sql, $warehouseId);
 		if ($rc === false) {
 			$db->rollback();
 			return $this->sqlError(__LINE__);
 		}
-		
+
 		$log = "仓库 [{$name}] 取消建账完毕标志";
 		$bs = new BizlogService();
 		$bs->insertBizlog($log, $this->LOG_CATEGORY);
-		
+
 		$db->commit();
-		
+
 		return $this->ok();
 	}
 }

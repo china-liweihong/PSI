685242cd68b87b3779b43e91e1db62eb2ff97127
CRM8000 <crm8000@qq.com>
2019-05-21 11:05:13 +0800

    系统数据字典

--- web/Application/Home/DAO/SysDictDAO.class.php
+++ web/Application/Home/DAO/SysDictDAO.class.php
@@ -14,12 +14,12 @@ class SysDictDAO extends PSIBaseExDAO {
 	 */
 	public function categoryList($params) {
 		$db = $this->db;
-		
+
 		$sql = "select id, code, name
 				from t_dict_table_category
 				order by code";
 		$data = $db->query($sql);
-		
+
 		$result = [];
 		foreach ( $data as $v ) {
 			$result[] = [
@@ -28,7 +28,7 @@ class SysDictDAO extends PSIBaseExDAO {
 					"name" => $v["name"]
 			];
 		}
-		
+
 		return $result;
 	}
 
@@ -37,17 +37,17 @@ class SysDictDAO extends PSIBaseExDAO {
 	 */
 	public function sysDictList($params) {
 		$db = $this->db;
-		
+
 		$categoryId = $params["categoryId"];
-		
+
 		$sql = "select id, code, name, table_name, memo
 				from t_dict_table_md
 				where category_id = '%s' 
 				order by code";
 		$data = $db->query($sql, $categoryId);
-		
+
 		$result = [];
-		
+
 		foreach ( $data as $v ) {
 			$result[] = [
 					"id" => $v["id"],
@@ -57,7 +57,7 @@ class SysDictDAO extends PSIBaseExDAO {
 					"memo" => $v["memo"]
 			];
 		}
-		
+
 		return $result;
 	}
 
@@ -66,26 +66,26 @@ class SysDictDAO extends PSIBaseExDAO {
 	 */
 	public function dictDataList($params) {
 		$db = $this->db;
-		
+
 		// 数据字典元数据id
 		$id = $params["id"];
-		
+
 		$sql = "select table_name from t_dict_table_md where id = '%s' ";
 		$data = $db->query($sql, $id);
 		if (! $data) {
 			return $this->emptyResult();
 		}
-		
+
 		$tableName = $data[0]["table_name"];
-		
+
 		$sql = "select id, code, code_int, name, memo
 				from %s 
-				order by code";
-		
+				order by show_order";
+
 		$data = $db->query($sql, $tableName);
-		
+
 		$result = [];
-		
+
 		foreach ( $data as $v ) {
 			$result[] = [
 					"id" => $v["id"],
@@ -95,7 +95,7 @@ class SysDictDAO extends PSIBaseExDAO {
 					"memo" => $v["memo"]
 			];
 		}
-		
+
 		return $result;
 	}
 }

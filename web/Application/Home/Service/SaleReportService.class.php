9c1759e85e807d97d42970a85e079d83879de0f8
CRM8000 <crm8000@qq.com>
2019-05-27 14:57:45 +0800

    销售日报表(按仓库汇总) - 增加每平方米销售额

--- web/Application/Home/Service/SaleReportService.class.php
+++ web/Application/Home/Service/SaleReportService.class.php
@@ -729,6 +729,7 @@ class SaleReportService extends PSIBaseExService {
 					<tr><td>仓库编码</td><td>仓库</td>
 						<td>销售出库金额</td><td>退货入库金额</td>
 						<td>净销售金额</td><td>毛利</td><td>毛利率</td>
+						<td>每平方米销售额</td><td>仓库销售核算面积</td>
 					</tr>
 				';
 		foreach ( $items as $v ) {
@@ -740,6 +741,8 @@ class SaleReportService extends PSIBaseExService {
 			$html .= '<td align="right">' . $v["m"] . '</td>';
 			$html .= '<td align="right">' . $v["profit"] . '</td>';
 			$html .= '<td align="right">' . $v["rate"] . '</td>';
+			$html .= '<td align="right">' . $v["saleAreaRate"] . '</td>';
+			$html .= '<td align="right">' . $v["saleArea"] . '</td>';
 			$html .= '</tr>';
 		}
 
@@ -814,6 +817,12 @@ class SaleReportService extends PSIBaseExService {
 		$sheet->getColumnDimension('G')->setWidth(15);
 		$sheet->setCellValue("G2", "毛利率");
 
+		$sheet->getColumnDimension('H')->setWidth(15);
+		$sheet->setCellValue("H2", "每平方米销售额");
+
+		$sheet->getColumnDimension('I')->setWidth(15);
+		$sheet->setCellValue("I2", "仓库销售核算面积");
+
 		foreach ( $items as $i => $v ) {
 			$row = $i + 3;
 			$sheet->setCellValue("A" . $row, $v["warehouseCode"]);
@@ -823,6 +832,8 @@ class SaleReportService extends PSIBaseExService {
 			$sheet->setCellValue("E" . $row, $v["m"]);
 			$sheet->setCellValue("F" . $row, $v["profit"]);
 			$sheet->setCellValue("G" . $row, $v["rate"]);
+			$sheet->setCellValue("H" . $row, $v["saleAreaRate"]);
+			$sheet->setCellValue("I" . $row, $v["saleArea"]);
 		}
 
 		// 画表格边框
@@ -834,7 +845,7 @@ class SaleReportService extends PSIBaseExService {
 				]
 		];
 		$lastRow = count($items) + 2;
-		$sheet->getStyle('A2:G' . $lastRow)->applyFromArray($styleArray);
+		$sheet->getStyle('A2:I' . $lastRow)->applyFromArray($styleArray);
 
 		$dt = date("YmdHis");
 

26107c57660dbb20254aaaa37a1c900ee3fddd32
CRM8000 <crm8000@qq.com>
2019-06-03 09:53:14 +0800

    一键升级数据库 - 成品委托生产订单增加含税价字段

--- web/Application/Home/Service/UpdateDBService.class.php
+++ web/Application/Home/Service/UpdateDBService.class.php
@@ -248,6 +248,7 @@ class UpdateDBService extends PSIBaseService {
 		$this->update_20190601_01();
 		$this->update_20190602_01();
 		$this->update_20190603_01();
+		$this->update_20190603_02();
 
 		$sql = "delete from t_psi_db_version";
 		$db->execute($sql);
@@ -268,6 +269,18 @@ class UpdateDBService extends PSIBaseService {
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	// ============================================
 	private function notForgot() {
+	}
+
+	private function update_20190603_02() {
+		// 本次更新：t_dmo_bill_detail新增字段goods_price_with_tax
+		$db = $this->db;
+
+		$tableName = "t_dmo_bill_detail";
+		$columnName = "goods_price_with_tax";
+		if (! $this->columnExists($db, $tableName, $columnName)) {
+			$sql = "alter table {$tableName} add {$columnName} decimal(19,2) DEFAULT NULL;";
+			$db->execute($sql);
+		}
 	}
 
 	private function update_20190603_01() {

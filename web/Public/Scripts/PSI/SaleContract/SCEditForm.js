7ecfb4d90c4ae8d44b35b0522ca1ccbaf3ebbba6
CRM8000 <crm8000@qq.com>
2019-06-10 19:57:12 +0800

    销售合同 - 处理含税价，利用ExtJS mix技术改进代码质量

--- web/Public/Scripts/PSI/SaleContract/SCEditForm.js
+++ web/Public/Scripts/PSI/SaleContract/SCEditForm.js
@@ -1,8 +1,10 @@
 //
-// 销售订单 - 新增或编辑界面
+// 销售合同 - 新增或编辑界面
 //
 Ext.define("PSI.SaleContract.SCEditForm", {
 	extend : "PSI.AFX.BaseDialogForm",
+
+	mixins : ["PSI.Mix.GoodsPrice"],
 
 	initComponent : function() {
 		var me = this;
@@ -861,88 +863,10 @@ Ext.define("PSI.SaleContract.SCEditForm", {
 				me.calcMoney2(goods);
 			}
 		}
-	},
 
-	calcTax : function(goods) {
-		if (!goods) {
-			return;
-		}
-		var taxRate = goods.get("taxRate") / 100;
-		var tax = goods.get("moneyWithTax") * taxRate / (1 + taxRate);
-		goods.set("tax", tax);
-		goods.set("goodsMoney", goods.get("moneyWithTax") - tax);
-
-		// 计算单价
-		if (goods.get("goodsCount") == 0) {
-			goods.set("goodsPrice", null);
-			goods.set("goodsPriceWithTax", null);
-		} else {
-			goods.set("goodsPrice", goods.get("goodsMoney")
-							/ goods.get("goodsCount"));
-			goods.set("goodsPriceWithTax", goods.get("moneyWithTax")
-							/ goods.get("goodsCount"));
-		}
-	},
-
-	calcMoneyWithTax : function(goods) {
-		if (!goods) {
-			return;
-		}
-		goods.set("goodsMoney", goods.get("tax") * 100 / goods.get("taxRate"));
-		goods.set("moneyWithTax", goods.get("goodsMoney") + goods.get("tax"));
-		if (goods.get("goodsCount") != 0) {
-			goods.set("goodsPrice", goods.get("goodsMoney")
-							/ goods.get("goodsCount"));
-			goods.set("goodsPriceWithTax", goods.get("moneyWithTax")
-							/ goods.get("goodsCount"));
-		}
-	},
-
-	// 因为不含税价格变化，重新计算金额
-	calcMoney : function(goods) {
-		if (!goods) {
-			return;
-		}
-
-		goods.set("goodsMoney", goods.get("goodsCount")
-						* goods.get("goodsPrice"));
-		goods.set("tax", goods.get("goodsMoney") * goods.get("taxRate") / 100);
-		goods.set("moneyWithTax", goods.get("goodsMoney") + goods.get("tax"));
-		if (goods.get("goodsCount") != 0) {
-			goods.set("goodsPriceWithTax", goods.get("moneyWithTax")
-							/ goods.get("goodsCount"));
-		}
-	},
-
-	// 因为含税价变化，重新计算金额
-	calcMoney2 : function(goods) {
-		if (!goods) {
-			return;
-		}
-
-		goods.set("moneyWithTax", goods.get("goodsPriceWithTax")
-						* goods.get("goodsCount"));
-		goods.set("goodsMoney", goods.get("moneyWithTax")
-						/ (1 + goods.get("taxRate") / 100));
-		goods.set("tax", goods.get("moneyWithTax") - goods.get("goodsMoney"));
-		if (goods.get("goodsCount") != 0) {
-			goods.set("goodsPrice", goods.get("goodsMoney")
-							/ goods.get("goodsCount"));
-		}
-	},
-
-	calcPrice : function(goods) {
-		if (!goods) {
-			return;
-		}
-
-		var goodsCount = goods.get("goodsCount");
-		if (goodsCount && goodsCount != 0) {
-			goods.set("goodsPrice", goods.get("goodsMoney")
-							/ goods.get("goodsCount"));
-			goods.set("goodsPriceWithTax", goods.get("moneyWithTax")
-							/ goods.get("goodsCount"));
-		}
+		// 上述代码的技术说明
+		// 各个calcXXXX函数实现在PSI.Mix.GoodsPrice中
+		// 这是利用ExtJS的mix技术
 	},
 
 	getSaveData : function() {

c8a0cbb48487935e9aa0410eb96c60ef4af2912e
CRM8000 <crm8000@qq.com>
2019-06-15 09:12:55 +0800

    代码格式调整

--- web/Public/Scripts/PSI/Sale/WSImportForm.js
+++ web/Public/Scripts/PSI/Sale/WSImportForm.js
@@ -4,89 +4,89 @@
  * @author 李静波
  */
 Ext.define("PSI.Sale.WSImportForm", {
-			extend : "Ext.window.Window",
-			config : {
-				parentForm : null
-			},
+  extend: "Ext.window.Window",
+  config: {
+    parentForm: null
+  },
 
-			initComponent : function() {
-				var me = this;
+  initComponent: function () {
+    var me = this;
 
-				Ext.apply(me, {
-							title : "导入销售出库单临时数据",
-							modal : true,
-							onEsc : Ext.emptyFn,
-							width : 400,
-							height : 420,
-							layout : "fit",
-							defaultFocus : "editData",
-							items : [{
-										id : "editForm",
-										xtype : "form",
-										layout : "form",
-										height : "100%",
-										bodyPadding : 5,
-										defaultType : 'textfield',
-										fieldDefaults : {
-											labelWidth : 60,
-											labelAlign : "right",
-											labelSeparator : "",
-											msgTarget : 'side'
-										},
-										items : [{
-													id : "editData",
-													fieldLabel : "临时数据",
-													xtype : "textareafield",
-													height : 300
-												}],
-										buttons : [{
-													text : "导入",
-													handler : me.onOK,
-													scope : me
-												}, {
-													text : "取消",
-													handler : function() {
-														me.close();
-													},
-													scope : me
-												}]
-									}],
-							listeners : {
-								show : {
-									fn : me.onWndShow,
-									scope : me
-								}
-							}
-						});
+    Ext.apply(me, {
+      title: "导入销售出库单临时数据",
+      modal: true,
+      onEsc: Ext.emptyFn,
+      width: 400,
+      height: 420,
+      layout: "fit",
+      defaultFocus: "editData",
+      items: [{
+        id: "editForm",
+        xtype: "form",
+        layout: "form",
+        height: "100%",
+        bodyPadding: 5,
+        defaultType: 'textfield',
+        fieldDefaults: {
+          labelWidth: 60,
+          labelAlign: "right",
+          labelSeparator: "",
+          msgTarget: 'side'
+        },
+        items: [{
+          id: "editData",
+          fieldLabel: "临时数据",
+          xtype: "textareafield",
+          height: 300
+        }],
+        buttons: [{
+          text: "导入",
+          handler: me.onOK,
+          scope: me
+        }, {
+          text: "取消",
+          handler: function () {
+            me.close();
+          },
+          scope: me
+        }]
+      }],
+      listeners: {
+        show: {
+          fn: me.onWndShow,
+          scope: me
+        }
+      }
+    });
 
-				me.callParent(arguments);
-			},
+    me.callParent(arguments);
+  },
 
-			onWndShow : function() {
-				var editName = Ext.getCmp("editData");
-				editName.focus();
-			},
+  onWndShow: function () {
+    var editName = Ext.getCmp("editData");
+    editName.focus();
+  },
 
-			onOK : function() {
-				var me = this;
+  onOK: function () {
+    var me = this;
 
-				var data = Ext.getCmp("editData").getValue();
-				if (!data) {
-					PSI.MsgBox.showInfo("没有输入导入数据");
-					return;
-				}
+    var data = Ext.getCmp("editData").getValue();
+    if (!data) {
+      PSI.MsgBox.showInfo("没有输入导入数据");
+      return;
+    }
 
-				var bill = Ext.JSON.decode(data, true);
+    var bill = Ext.JSON.decode(data, true);
 
-				if (!bill) {
-					PSI.MsgBox.showInfo("输入的数据不正确，无法导入");
-					return;
-				}
+    if (!bill) {
+      PSI.MsgBox.showInfo("输入的数据不正确，无法导入");
+      return;
+    }
 
-				var result = me.getParentForm().importBill(bill);
+    var result = me.getParentForm().importBill(bill);
 
-				if (result) {
-					me.close();
-				}
-			}
-		});
+    if (result) {
+      me.close();
+    }
+  }
+});

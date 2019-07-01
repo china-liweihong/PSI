80993a8703c44ca48972aba34e6977cbf70fce1e
CRM8000 <crm8000@qq.com>
2019-07-01 19:59:17 +0800

    码表设置

---
+++ web/Public/Scripts/PSI/CodeTable/CodeTableParentIdField.js
@@ -0,0 +1,139 @@
+/**
+ * 自定义字段 - 上级字段
+ */
+Ext.define("PSI.CodeTable.CodeTableParentIdField", {
+  extend: "Ext.form.field.Trigger",
+  alias: "widget.psi_codetable_parentidfield",
+
+  initComponent: function () {
+    var me = this;
+
+    me.__idValue = null;
+
+    me.enableKeyEvents = true;
+
+    me.callParent(arguments);
+
+    me.on("keydown", function (field, e) {
+      if (e.getKey() == e.BACKSPACE) {
+        field.setValue(null);
+        me.setIdValue(null);
+        e.preventDefault();
+        return false;
+      }
+
+      if (e.getKey() !== e.ENTER) {
+        this.onTriggerClick(e);
+      }
+    });
+
+    me.on({
+      render: function (p) {
+        p.getEl().on("dblclick", function () {
+          me.onTriggerClick();
+        });
+      },
+      single: true
+    });
+  },
+
+  onTriggerClick: function (e) {
+    var modelName = "PSICodeTableParentIdField";
+    Ext.define(modelName, {
+      extend: "Ext.data.Model",
+      fields: ["id", "text", "fullName", "code",
+        "leaf", "children"]
+    });
+
+    var store = Ext.create("Ext.data.TreeStore", {
+      model: modelName,
+      proxy: {
+        type: "ajax",
+        extraParams: {
+        },
+        actionMethods: {
+          read: "POST"
+        },
+        url: PSI.Const.BASE_URL + "Home/CodeTable/getDataForParentIdField"
+      }
+    });
+
+    var tree = Ext.create("Ext.tree.Panel", {
+      cls: "PSI",
+      store: store,
+      rootVisible: false,
+      useArrows: true,
+      viewConfig: {
+        loadMask: true
+      },
+      columns: {
+        defaults: {
+          flex: 1,
+          sortable: false,
+          menuDisabled: true,
+          draggable: false
+        },
+        items: [{
+          xtype: "treecolumn",
+          text: "名称",
+          dataIndex: "text"
+        }, {
+          text: "编码",
+          dataIndex: "code"
+        }]
+      }
+    });
+    orgTree.on("itemdblclick", this.onOK, this);
+    this.tree = orgTree;
+
+    var wnd = Ext.create("Ext.window.Window", {
+      title: "选择组织机构",
+      modal: true,
+      width: 400,
+      height: 300,
+      layout: "fit",
+      items: [tree],
+      buttons: [{
+        text: "确定",
+        handler: this.onOK,
+        scope: this
+      }, {
+        text: "取消",
+        handler: function () {
+          wnd.close();
+        }
+      }]
+    });
+    this.wnd = wnd;
+    wnd.show();
+  },
+
+  // private
+  onOK: function () {
+    var me = this;
+
+    var tree = me.tree;
+    var item = tree.getSelectionModel().getSelection();
+
+    if (item === null || item.length !== 1) {
+      PSI.MsgBox.showInfo("没有选择记录");
+
+      return;
+    }
+
+    var data = item[0];
+
+    me.setIdValue(data.get("id"));
+    me.setValue(data.get("fullName"));
+    me.wnd.close();
+    me.focus();
+  },
+
+  setIdValue: function (id) {
+    this.__idValue = id;
+  },
+
+  getIdValue: function () {
+    return this.__idValue;
+  }
+});

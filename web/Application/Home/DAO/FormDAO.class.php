25cd98b15c127da59fca23d3d1a4303ac33e9d2c
CRM8000 <crm8000@qq.com>
2019-08-09 10:53:39 +0800

    自定义表单

--- web/Application/Home/DAO/FormDAO.class.php
+++ web/Application/Home/DAO/FormDAO.class.php
@@ -41,6 +41,41 @@ class FormDAO extends PSIBaseExDAO
   {
     $db = $this->db;
 
-    return $this->todo();
+    $code = $params["code"] ?? "";
+    $code = strtoupper($code);
+    $name = $params["name"];
+
+    // 检查编码是否存在
+    if ($code) {
+      $sql = "select count(*) as cnt from t_form_category where code = '%s' ";
+      $data = $db->query($sql, $code);
+      $cnt = $data[0]["cnt"];
+      if ($cnt) {
+        return $this->bad("表单分类编码[{$code}]已经存在");
+      }
+    } else {
+      $code = "";
+    }
+
+    // 检查分类名称是否存在
+    $sql = "select count(*) as cnt from t_form_category where name = '%s' ";
+    $data = $db->query($sql, $name);
+    $cnt = $data[0]["cnt"];
+    if ($cnt) {
+      return $this->bad("表单分类[{$name}]已经存在");
+    }
+
+    $id = $this->newId();
+    $sql = "insert into t_form_category (id, code, name, parent_id)
+            values ('%s', '%s', '%s', null)";
+
+    $rc = $db->execute($sql, $id, $code, $name);
+    if ($rc === false) {
+      return $this->sqlError(__METHOD__, __LINE__);
+    }
+
+    // 操作成功
+    $params["id"] = $id;
+    return null;
   }
 }

7cbff35ac112b28a286296ee59ca1278f2cf55c2
CRM8000 <crm8000@qq.com>
2019-11-22 09:52:30 +0800

    更新数据库文档 - t_form新增字段sys_form

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -1523,6 +1523,7 @@ CREATE TABLE IF NOT EXISTS `t_form` (
   `code` varchar(255) NOT NULL,
   `name` varchar(1000) NOT NULL,
   `category_id` varchar(255) NOT NULL,
+  `sys_form` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 

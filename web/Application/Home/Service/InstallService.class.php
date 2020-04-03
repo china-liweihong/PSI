636124c536919c74fd678f691a0411afcb97c564
CRM8000 <crm8000@qq.com>
2020-04-03 19:24:24 +0800

    更新数据库文档 - 新增表t_voucher_detail

--- web/Application/Home/Service/InstallService.class.php
+++ web/Application/Home/Service/InstallService.class.php
@@ -1849,6 +1849,20 @@ class InstallService extends PSIBaseExService
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
             ";
     $db->execute($sql);
+
+    // t_voucher_detail
+    $sql = "CREATE TABLE IF NOT EXISTS `t_voucher_detail` (
+              `id` varchar(255) NOT NULL,
+              `voucher_id` varchar(255) NOT NULL,
+              `subject` varchar(255) NOT NULL,
+              `summary` varchar(255) DEFAULT NULL,
+              `debit` decimal(19, 2) DEFAULT NULL,
+              `credit` decimal(19, 2) DEFAULT NULL,
+              `show_order` int(11) NOT NULL,
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+            ";
+    $db->execute($sql);
   }
 
   /**

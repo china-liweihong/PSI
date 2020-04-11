086dda58ddf46f41f34adfac8aeb6b3823ef8b04
CRM8000 <crm8000@qq.com>
2020-04-11 15:40:50 +0800

    物料单位

---
+++ web/Application/Home/DAO/MaterialDAO.class.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Home\DAO;
+
+/**
+ * 物料 DAO
+ *
+ * @author 李静波
+ */
+class MaterialDAO extends PSIBaseExDAO
+{
+}
---
+++ web/Application/Home/Service/MaterialService.class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Home\Service;
+
+/**
+ * 物料Service
+ *
+ * @author 李静波
+ */
+class MaterialService extends PSIBaseExService
+{
+  private $LOG_CATEGORY = "物料";
+}

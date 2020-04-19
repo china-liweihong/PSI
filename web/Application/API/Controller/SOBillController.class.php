849be8d84ce894ebc0681ec9934a57234d4e6ab7
CRM8000 <crm8000@qq.com>
2020-04-19 14:53:24 +0800

    修正bug：移动端-销售订单不能查看到现金付款的订单，参见： #I1EWID

--- web/Application/API/Controller/SOBillController.class.php
+++ web/Application/API/Controller/SOBillController.class.php
@@ -11,39 +11,43 @@ use Think\Controller;
  * @author 李静波
  *        
  */
-class SOBillController extends Controller {
+class SOBillController extends Controller
+{
 
-	/**
-	 * 销售订单列表
-	 */
-	public function sobillList() {
-		if (IS_POST) {
-			$params = [
-					"tokenId" => I("post.tokenId"),
-					"page" => I("post.page"),
-					"limit" => 10,
-					"billStatus" => - 1
-			];
-			
-			$service = new SOBillApiService();
-			
-			$this->ajaxReturn($service->sobillList($params));
-		}
-	}
+  /**
+   * 销售订单列表
+   */
+  public function sobillList()
+  {
+    if (IS_POST) {
+      $params = [
+        "tokenId" => I("post.tokenId"),
+        "page" => I("post.page"),
+        "limit" => 10,
+        "billStatus" => -1,
+        "receivingType" => -1,
+      ];
 
-	/**
-	 * 某个采购订单的详情
-	 */
-	public function sobillInfo() {
-		if (IS_POST) {
-			$params = [
-					"tokenId" => I("post.tokenId"),
-					"id" => I("post.id")
-			];
-			
-			$service = new SOBillApiService();
-			
-			$this->ajaxReturn($service->sobillInfo($params));
-		}
-	}
+      $service = new SOBillApiService();
+
+      $this->ajaxReturn($service->sobillList($params));
+    }
+  }
+
+  /**
+   * 某个采购订单的详情
+   */
+  public function sobillInfo()
+  {
+    if (IS_POST) {
+      $params = [
+        "tokenId" => I("post.tokenId"),
+        "id" => I("post.id")
+      ];
+
+      $service = new SOBillApiService();
+
+      $this->ajaxReturn($service->sobillInfo($params));
+    }
+  }
 }

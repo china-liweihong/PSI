53e9f68a1267a0ef4e1019437e0b696449637869
CRM8000 <crm8000@qq.com>
2020-04-23 12:34:30 +0800

    更新数据库文档 - 系统固有码表分类

--- doc/99 SQL/02InsertInitData.sql
+++ doc/99 SQL/02InsertInitData.sql
@@ -606,13 +606,23 @@ INSERT INTO `t_sysdict_form_editor_xtype` (`id`, `code`, `code_int`, `name`, `py
 ('BF6F569E-843D-11EA-8C00-E86A641ED142', '4', 4, 'displayfield', 'displayfield', '不使用编辑器', 4),
 ('CD0B859B-843D-11EA-8C00-E86A641ED142', '5', 5, 'psi_userfield', 'psi_userfield', '用户字段编辑器', 5);
 
-TRUNCATE TABLE `t_code_table_category`;
+DELETE FROM `t_code_table_category` where `id` = 'F9D80BD6-8519-11EA-B071-E86A641ED142';
 INSERT INTO `t_code_table_category` (`id`, `code`, `name`, `parent_id`, `is_system`) VALUES
-('F9D80BD6-8519-11EA-B071-E86A641ED142', 'PSI-0001', '用户', NULL, 1),
-('58BF84A3-8517-11EA-B071-E86A641ED142', 'PSI-0002', '商品', NULL, 1),
-('05717096-851A-11EA-B071-E86A641ED142', 'PSI-0003', '仓库', NULL, 1),
-('0F8C175C-851A-11EA-B071-E86A641ED142', 'PSI-0004', '供应商', NULL, 1),
-('19DFD9E7-851A-11EA-B071-E86A641ED142', 'PSI-0005', '客户', NULL, 1),
+('F9D80BD6-8519-11EA-B071-E86A641ED142', 'PSI-0001', '用户', NULL, 1);
+DELETE FROM `t_code_table_category` where `id` = '58BF84A3-8517-11EA-B071-E86A641ED142';
+INSERT INTO `t_code_table_category` (`id`, `code`, `name`, `parent_id`, `is_system`) VALUES
+('58BF84A3-8517-11EA-B071-E86A641ED142', 'PSI-0002', '商品', NULL, 1);
+DELETE FROM `t_code_table_category` where `id` = '05717096-851A-11EA-B071-E86A641ED142';
+INSERT INTO `t_code_table_category` (`id`, `code`, `name`, `parent_id`, `is_system`) VALUES
+('05717096-851A-11EA-B071-E86A641ED142', 'PSI-0003', '仓库', NULL, 1);
+DELETE FROM `t_code_table_category` where `id` = '0F8C175C-851A-11EA-B071-E86A641ED142';
+INSERT INTO `t_code_table_category` (`id`, `code`, `name`, `parent_id`, `is_system`) VALUES
+('0F8C175C-851A-11EA-B071-E86A641ED142', 'PSI-0004', '供应商', NULL, 1);
+DELETE FROM `t_code_table_category` where `id` = '19DFD9E7-851A-11EA-B071-E86A641ED142';
+INSERT INTO `t_code_table_category` (`id`, `code`, `name`, `parent_id`, `is_system`) VALUES
+('19DFD9E7-851A-11EA-B071-E86A641ED142', 'PSI-0005', '客户', NULL, 1);
+DELETE FROM `t_code_table_category` where `id` = '2FCB8D75-851A-11EA-B071-E86A641ED142';
+INSERT INTO `t_code_table_category` (`id`, `code`, `name`, `parent_id`, `is_system`) VALUES
 ('2FCB8D75-851A-11EA-B071-E86A641ED142', 'PSI-0006', '工厂', NULL, 1);
 
 

3e98eb30eb2eb595da9fe49b86daf2b64e0e03a5
CRM8000 <crm8000@qq.com>
2020-04-24 16:44:46 +0800

    更新数据库文档 - 初始化t_warehouse码表元数据

--- doc/99 SQL/02InsertInitData.sql
+++ doc/99 SQL/02InsertInitData.sql
@@ -663,6 +663,24 @@ INSERT INTO `t_code_table_cols_md` (`id`, `table_id`, `caption`, `db_field_name`
 ('1C7C1E35-85CC-11EA-A819-E86A641ED142', '1C7AE1C9-85CC-11EA-A819-E86A641ED142', '最后一次编辑人id', 'update_user_id', 'varchar', 255, 0, -696, 1, '', '', '', 1, 1, 2, 0, NULL, -1000, 'textfield'),
 ('1C7C3324-85CC-11EA-A819-E86A641ED142', '1C7AE1C9-85CC-11EA-A819-E86A641ED142', '状态', 'record_status', 'int', 11, 0, 2, 2, 't_sysdict_record_status', 'code_int', 'name', 2, 1, 1, 80, NULL, 2, 'textfield');
 
+#码表：t_warehouse
+DELETE FROM `t_code_table_md` where `id` = '49F3F27F-8607-11EA-A0E2-E86A641ED142';
+DELETE FROM `t_code_table_cols_md` where `table_id` = '49F3F27F-8607-11EA-A0E2-E86A641ED142' and `sys_col` = 1;
+INSERT INTO `t_code_table_md` (`id`, `code`, `name`, `table_name`, `category_id`, `memo`, `py`, `fid`, `md_version`, `is_fixed`, `enable_parent_id`, `handler_class_name`) VALUES
+('49F3F27F-8607-11EA-A0E2-E86A641ED142', 'PSI-0003-01', '仓库', 't_warehouse', '05717096-851A-11EA-B071-E86A641ED142', '', '', '', 1, 1, 0, '');
+INSERT INTO `t_code_table_cols_md` (`id`, `table_id`, `caption`, `db_field_name`, `db_field_type`, `db_field_length`, `db_field_decimal`, `show_order`, `value_from`, `value_from_table_name`, `value_from_col_name`, `value_from_col_name_display`, `must_input`, `sys_col`, `is_visible`, `width_in_view`, `note`, `show_order_in_view`, `editor_xtype`) VALUES
+('49F42B5E-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', 'id', 'id', 'varchar', 255, 0, -1000, 1, '', '', '', 1, 1, 2, 0, NULL, -1000, 'textfield'),
+('49F43F2A-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', '编码', 'code', 'varchar', 255, 0, 0, 1, '', '', '', 2, 1, 1, 120, NULL, 0, 'textfield'),
+('49F44FFE-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', '名称', 'name', 'varchar', 255, 0, 1, 1, '', '', '', 2, 1, 1, 200, NULL, 1, 'textfield'),
+('49F45D67-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', '拼音字头', 'py', 'varchar', 255, 0, -900, 1, '', '', '', 1, 1, 2, 0, NULL, -1000, 'textfield'),
+('49F46C4D-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', '数据域', 'data_org', 'varchar', 255, 0, -800, 1, '', '', '', 1, 1, 2, 0, NULL, -1000, 'textfield'),
+('49F47EF1-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', '公司id', 'company_id', 'varchar', 255, 0, -700, 1, '', '', '', 1, 1, 2, 0, NULL, -1000, 'textfield'),
+('49F48D39-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', '记录创建时间', 'date_created', 'datetime', 0, 0, -699, 1, '', '', '', 1, 1, 2, 0, NULL, -1000, 'textfield'),
+('49F49D88-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', '记录创建人id', 'create_user_id', 'varchar', 255, 0, -698, 1, '', '', '', 1, 1, 2, 0, NULL, -1000, 'textfield'),
+('49F4AB83-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', '最后一次编辑时间', 'update_dt', 'datetime', 0, 0, -697, 1, '', '', '', 1, 1, 2, 0, NULL, -1000, 'textfield'),
+('49F4B946-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', '最后一次编辑人id', 'update_user_id', 'varchar', 255, 0, -696, 1, '', '', '', 1, 1, 2, 0, NULL, -1000, 'textfield'),
+('49F4C69A-8607-11EA-A0E2-E86A641ED142', '49F3F27F-8607-11EA-A0E2-E86A641ED142', '状态', 'record_status', 'int', 11, 0, 2, 2, 't_sysdict_record_status', 'code_int', 'name', 2, 1, 1, 80, NULL, 2, 'textfield');
+
 
 TRUNCATE TABLE `t_psi_db_version`;
 INSERT INTO `t_psi_db_version` (`db_version`, `update_dt`) VALUES

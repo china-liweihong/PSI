2306979e561c191ed9c9b55d569c269c7044b04b
CRM8000 <crm8000@qq.com>
2020-04-27 11:31:53 +0800

    码表

--- web/Application/Home/DAO/CodeTableDAO.class.php
+++ web/Application/Home/DAO/CodeTableDAO.class.php
@@ -2209,6 +2209,7 @@ class CodeTableDAO extends PSIBaseExDAO
       ["id" => "49F3F27F-8607-11EA-A0E2-E86A641ED142", "tableName" => "t_warehouse"],
       ["id" => "C68DBABE-860B-11EA-A0E2-E86A641ED142", "tableName" => "t_goods_category"],
       ["id" => "8BD2B19E-8623-11EA-B463-E86A641ED142", "tableName" => "t_goods_unit"],
+      ["id" => "D7D9D328-8834-11EA-8C36-E86A641ED142", "tableName" => "t_goods_brand"],
     ];
 
     foreach ($list as $v) {

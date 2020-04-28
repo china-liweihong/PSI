e9916e2362160db9c766581765dc86da79ad0e27
CRM8000 <crm8000@qq.com>
2020-04-28 17:51:18 +0800

    更新数据库文档 - t_code_table_md新增字段module_name

--- doc/99 SQL/01CreateTables.sql
+++ doc/99 SQL/01CreateTables.sql
@@ -1417,6 +1417,7 @@ CREATE TABLE IF NOT EXISTS `t_code_table_md` (
   `is_fixed` int(11) NOT NULL DEFAULT 2,
   `enable_parent_id` int(11) NOT NULL DEFAULT 0,
   `handler_class_name` varchar(255) DEFAULT NULL,
+  `module_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 

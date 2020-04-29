9e5fae5c8a1918a032fc768385df901abc4d6ded
CRM8000 <crm8000@qq.com>
2020-04-29 16:00:48 +0800

    码表

--- web/Public/Scripts/PSI/CodeTable/CodeTableEditForm.js
+++ web/Public/Scripts/PSI/CodeTable/CodeTableEditForm.js
@@ -24,26 +24,15 @@ Ext.define("PSI.CodeTable.CodeTableEditForm", {
       },
       scope: me
     }, {
-        text: entity == null ? "关闭" : "取消",
-        handler: function () {
-          me.close();
-        },
-        scope: me
-      });
+      text: entity == null ? "关闭" : "取消",
+      handler: function () {
+        me.close();
+      },
+      scope: me
+    });
 
     var t = entity == null ? "新增码表" : "编辑码表";
-    var f = entity == null
-      ? "edit-form-create.png"
-      : "edit-form-update.png";
-    var logoHtml = "<img style='float:left;margin:10px 20px 0px 10px;width:48px;height:48px;' src='"
-      + PSI.Const.BASE_URL
-      + "Public/Images/"
-      + f
-      + "'></img>"
-      + "<h2 style='color:#196d83'>"
-      + t
-      + "</h2>"
-      + "<p style='color:#196d83'>标记 <span style='color:red;font-weight:bold'>*</span>的是必须录入数据的字段</p>";
+    var logoHtml = me.genLogoHtml(entity, t);
 
     Ext.apply(me, {
       header: {

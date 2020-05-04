19579b3bb272d18c321a4d8109568c98440362f6
CRM8000 <crm8000@qq.com>
2020-05-04 12:05:43 +0800

    视图开发助手

--- web/Public/Scripts/PSI/FormView/MainForm.js
+++ web/Public/Scripts/PSI/FormView/MainForm.js
@@ -13,14 +13,14 @@ Ext.define("PSI.FormView.MainForm", {
         border: 0,
         split: true,
         region: "west",
-        width: "40%",
+        width: 370,
         layout: "fit",
-        items: []
+        items: me.getCategoryGrid()
       }, {
         border: 0,
         region: "center",
         layout: "fit",
-        items:[]
+        items: []
       }]
     });
 
@@ -36,5 +36,71 @@ Ext.define("PSI.FormView.MainForm", {
       },
       scope: me
     }];
+  },
+
+  getCategoryGrid: function () {
+    var me = this;
+
+    if (me.__categoryGrid) {
+      return me.__categoryGrid;
+    }
+
+    var modelName = "PSIFvCategory";
+
+    Ext.define(modelName, {
+      extend: "Ext.data.Model",
+      fields: ["id", "code", "name", "isSystem", "isSystemCaption"]
+    });
+
+    me.__categoryGrid = Ext.create("Ext.grid.Panel", {
+      cls: "PSI",
+      viewConfig: {
+        enableTextSelection: true
+      },
+      header: {
+        height: 30,
+        title: me.formatGridHeaderTitle("视图分类")
+      },
+      tools: [{
+        type: "close",
+        handler: function () {
+          Ext.getCmp("panelCategory").collapse();
+        }
+      }],
+      columnLines: true,
+      columns: [{
+        header: "分类编码",
+        dataIndex: "code",
+        width: 80,
+        menuDisabled: true,
+        sortable: false
+      }, {
+        header: "视图分类",
+        dataIndex: "name",
+        width: 200,
+        menuDisabled: true,
+        sortable: false
+      }, {
+        header: "系统固有",
+        dataIndex: "isSystemCaption",
+        menuDisabled: true,
+        width: 80,
+        align: "center",
+        sortable: false
+      }],
+      store: Ext.create("Ext.data.Store", {
+        model: modelName,
+        autoLoad: false,
+        data: []
+      }),
+      listeners: {
+        select: {
+          fn: me.onCategoryGridSelect,
+          scope: me
+        }
+      }
+    });
+
+    return me.__categoryGrid;
   }
 });

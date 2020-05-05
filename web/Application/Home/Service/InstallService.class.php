7af60c9c20e0d3c1c98937e1cab48de291761bba
CRM8000 <crm8000@qq.com>
2020-05-05 10:06:50 +0800

    更新数据库文档 - t_fv新增三个字段：xtype、region和width_or_height

--- web/Application/Home/Service/InstallService.class.php
+++ web/Application/Home/Service/InstallService.class.php
@@ -1980,6 +1980,9 @@ class InstallService extends PSIBaseExService
               `is_fixed` int(11) NOT NULL DEFAULT 2,
               `parent_id` varchar(255) DEFAULT NULL,
               `module_name` varchar(255) DEFAULT NULL,
+              `xtype` varchar(255) NOT NULL,
+              `region` varchar(255) DEFAULT NULL,
+              `width_or_height` varchar(255) DEFAULT NULL,
               PRIMARY KEY (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
             ";

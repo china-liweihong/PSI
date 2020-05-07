c7b1ebe3d6172ff94102264ea5d09539a16c499d
CRM8000 <crm8000@qq.com>
2020-05-07 09:18:25 +0800

    视图开发助手

---
+++ web/Public/Scripts/PSI/FormView/FvEditForm.js
@@ -0,0 +1,283 @@
+//
+// 视图 - 新建或编辑界面
+//
+Ext.define("PSI.FormView.FvEditForm", {
+  extend: "PSI.AFX.BaseDialogForm",
+
+  config: {
+    category: null
+  },
+
+  initComponent: function () {
+    var me = this;
+    var entity = me.getEntity();
+    this.adding = entity == null;
+
+    var buttons = [];
+
+    buttons.push({
+      text: "保存",
+      formBind: true,
+      iconCls: "PSI-button-ok",
+      handler: function () {
+        me.onOK(false);
+      },
+      scope: me
+    }, {
+      text: entity == null ? "关闭" : "取消",
+      handler: function () {
+        me.close();
+      },
+      scope: me
+    });
+
+    var t = entity == null ? "新增视图" : "编辑视图";
+    var logoHtml = me.genLogoHtml(entity, t);
+
+    Ext.apply(me, {
+      header: {
+        title: me.formatTitle(PSI.Const.PROD_NAME),
+        height: 40
+      },
+      width: 550,
+      height: 370,
+      layout: "border",
+      items: [{
+        region: "north",
+        border: 0,
+        height: 90,
+        html: logoHtml
+      }, {
+        region: "center",
+        border: 0,
+        id: "PSI_FormView_FvEditForm_editForm",
+        xtype: "form",
+        layout: {
+          type: "table",
+          columns: 2
+        },
+        height: "100%",
+        bodyPadding: 5,
+        defaultType: 'textfield',
+        fieldDefaults: {
+          labelWidth: 100,
+          labelAlign: "right",
+          labelSeparator: "",
+          msgTarget: 'side'
+        },
+        items: [{
+          xtype: "hidden",
+          name: "id",
+          value: entity == null ? null : entity.get("id")
+        }, {
+          id: "PSI_FormView_FvEditForm_editCategoryId",
+          xtype: "hidden",
+          name: "categoryId"
+        }, {
+          id: "PSI_FormView_FvEditForm_editCategory",
+          xtype: "psi_fvcategoryfield",
+          fieldLabel: "分类",
+          allowBlank: false,
+          blankText: "没有输入视图分类",
+          beforeLabelTextTpl: PSI.Const.REQUIRED,
+          valueField: "id",
+          displayField: "name",
+          listeners: {
+            specialkey: {
+              fn: me.onEditSpecialKey,
+              scope: me
+            }
+          }
+        }, {
+          id: "PSI_FormView_FvEditForm_editCode",
+          fieldLabel: "编码",
+          name: "code",
+          listeners: {
+            specialkey: {
+              fn: me.onEditSpecialKey,
+              scope: me
+            }
+          }
+        }, {
+          id: "PSI_FormView_FvEditForm_editName",
+          fieldLabel: "视图名称",
+          allowBlank: false,
+          blankText: "没有输入中文名称",
+          beforeLabelTextTpl: PSI.Const.REQUIRED,
+          name: "name",
+          listeners: {
+            specialkey: {
+              fn: me.onEditSpecialKey,
+              scope: me
+            }
+          }
+        }, {
+          id: "PSI_FormView_FvEditForm_editModuleName",
+          fieldLabel: "模块名称",
+          allowBlank: false,
+          blankText: "没有输入模块名称",
+          beforeLabelTextTpl: PSI.Const.REQUIRED,
+          name: "moduleName",
+          listeners: {
+            specialkey: {
+              fn: me.onEditSpecialKey,
+              scope: me
+            }
+          }
+        }, {
+          id: "PSI_CodeTable_CodeTableEditForm_editMemo",
+          fieldLabel: "备注",
+          name: "memo",
+          value: entity == null ? null : entity.get("note"),
+          listeners: {
+            specialkey: {
+              fn: me.onEditLastSpecialKey,
+              scope: me
+            }
+          },
+          width: 510,
+          colspan: 2
+        }],
+        buttons: buttons
+      }],
+      listeners: {
+        show: {
+          fn: me.onWndShow,
+          scope: me
+        },
+        close: {
+          fn: me.onWndClose,
+          scope: me
+        }
+      }
+    });
+
+    me.callParent(arguments);
+
+    me.editForm = Ext.getCmp("PSI_FormView_FvEditForm_editForm");
+
+    me.editCategoryId = Ext.getCmp("PSI_FormView_FvEditForm_editCategoryId");
+    me.editCategory = Ext.getCmp("PSI_FormView_FvEditForm_editCategory");
+    me.editCode = Ext.getCmp("PSI_FormView_FvEditForm_editCode");
+    me.editName = Ext.getCmp("PSI_FormView_FvEditForm_editName");
+    me.editModuleName = Ext.getCmp("PSI_FormView_FvEditForm_editModuleName");
+    me.editMemo = Ext.getCmp("PSI_FormView_FvEditForm_editMemo");
+
+    me.__editorList = [
+      me.editCategory, me.editCode, me.editName, me.editModuleName,
+      me.editMemo];
+
+    var c = me.getCategory();
+    if (c) {
+      me.editCategory.setIdValue(c.get("id"));
+      me.editCategory.setValue(c.get("name"));
+    }
+  },
+
+  onWndShow: function () {
+    var me = this;
+
+    Ext.get(window).on('beforeunload', me.onWindowBeforeUnload);
+
+    if (me.adding) {
+      // 新建
+    } else {
+      // 编辑
+      var el = me.getEl();
+      el && el.mask(PSI.Const.LOADING);
+      Ext.Ajax.request({
+        url: me.URL("Home/FormView/fvInfo"),
+        params: {
+          id: me.getEntity().get("id")
+        },
+        method: "POST",
+        callback: function (options, success, response) {
+          if (success) {
+            var data = Ext.JSON.decode(response.responseText);
+            me.editCategory.setIdValue(data.categoryId);
+            me.editCategory.setValue(data.categoryName);
+            me.editCode.setValue(data.code);
+            me.editName.setValue(data.name);
+            me.editModuleName.setValue(data.moduleName);
+            me.editMemo.setValue(data.memo);
+          }
+
+          el && el.unmask();
+        }
+      });
+    }
+
+    me.editCode.focus();
+    me.editCode.setValue(me.editCode.getValue());
+  },
+
+  onOK: function () {
+    var me = this;
+
+    me.editCategoryId.setValue(me.editCategory.getIdValue());
+
+    var f = me.editForm;
+    var el = f.getEl();
+    el && el.mask(PSI.Const.SAVING);
+    f.submit({
+      url: me.URL("Home/FormView/editFv"),
+      method: "POST",
+      success: function (form, action) {
+        el && el.unmask();
+        PSI.MsgBox.tip("数据保存成功");
+        me.focus();
+        me.__lastId = action.result.id;
+        me.close();
+      },
+      failure: function (form, action) {
+        el && el.unmask();
+        PSI.MsgBox.showInfo(action.result.msg, function () {
+          me.editCode.focus();
+        });
+      }
+    });
+  },
+
+  onEditSpecialKey: function (field, e) {
+    var me = this;
+
+    if (e.getKey() === e.ENTER) {
+      var id = field.getId();
+      for (var i = 0; i < me.__editorList.length; i++) {
+        var edit = me.__editorList[i];
+        if (id == edit.getId()) {
+          var edit = me.__editorList[i + 1];
+          edit.focus();
+          edit.setValue(edit.getValue());
+        }
+      }
+    }
+  },
+
+  onEditLastSpecialKey: function (field, e) {
+    var me = this;
+
+    if (e.getKey() === e.ENTER) {
+      var f = me.editForm;
+      if (f.getForm().isValid()) {
+        me.onOK();
+      }
+    }
+  },
+
+  onWindowBeforeUnload: function (e) {
+    return (window.event.returnValue = e.returnValue = '确认离开当前页面？');
+  },
+
+  onWndClose: function () {
+    var me = this;
+
+    Ext.get(window).un('beforeunload', me.onWindowBeforeUnload);
+
+    if (me.__lastId) {
+      if (me.getParentForm()) {
+        me.getParentForm().refreshMainGrid(me.__lastId);
+      }
+    }
+  }
+});
